25e85c23905457b95c4a2f422fe1f52bbd9354f2
ignatius21 <ignaciojosealberto@hotmail.com>
2024-04-05 21:37:57 -0300

    Actualizar página de reunión y agregar dependencias necesarias

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "Live-Event",
+  description: "Speek with your friends and family in a live event",
+  icons: {
+    icon:'/icons/logo.svg',
+  }
+};
 
 const HomeLayout = ({children}:{children: ReactNode}) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "Live-Event",
+  description: "Speek with your friends and family in a live event",
+  icons: {
+    icon:'/icons/logo.svg',
+  }
+};
 
 const RootLayout = ({children}:{children: ReactNode}) => {
   return (
