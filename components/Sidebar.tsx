64368140647cf6b24fa340397021a28fede7900e
ignatius21 <ignaciojosealberto@hotmail.com>
2024-04-07 12:37:56 -0300

    Agregada funcionalidad de animación en Sidebar.tsx y actualizado package.json y package-lock.json

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import {motion} from 'framer-motion'
 
 const Sidebar = () => {
     const pathname = usePathname();
@@ -16,8 +17,10 @@ const Sidebar = () => {
                 const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
                 return (
                     <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{'bg-blue-1': isActive})}>
-                        <Image src={link.imgUrl} alt={link.label} width={24} height={24}/>
-                        <p className='text-lg font-semibold max-lg:hidden'>{link.label}</p>
+                        <motion.div className='flex gap-4' whileHover={{scale: 1.055,transition: { duration: 0.3 },}}>
+                            <Image src={link.imgUrl} alt={link.label} width={24} height={24}/>
+                            <p className='text-lg font-semibold max-lg:hidden hover:text-sky-2'>{link.label}</p>
+                        </motion.div>
                     </Link>
                 )
             })}
