aba8cf5f3f797bebace1eaf94b29eadd74d0cb86
ignatius21 <ignaciojosealberto@hotmail.com>
2024-04-07 21:08:41 -0300

    Actualizado StreamClientProvider.tsx para corregir el nombre de usuario

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -20,7 +20,7 @@ const StreamVideoProvider = ({children}:{children: ReactNode}) => {
             apiKey,
             user: {
                 id: user?.id,
-                name: user?.username || user?.id,
+                name: user?.firstName || user?.firstName || user?.id,
                 image: user?.imageUrl,
             },
             tokenProvider,
